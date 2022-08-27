7904a28002b0cc50456973c3bc5b9cd95d8faa3c
namiyazhang <namiyazhang@namiyazhangdeMacBook-Pro.local>
2022-08-27 18:06:08 +0800

    [init] cs61a

---
+++ cs61a/lab/lab01/lab01.py
@@ -0,0 +1,70 @@
+def falling(n, k):
+    """Compute the falling factorial of n to depth k.
+
+    >>> falling(6, 3)  # 6 * 5 * 4
+    120
+    >>> falling(4, 3)  # 4 * 3 * 2
+    24
+    >>> falling(4, 1)  # 4
+    4
+    >>> falling(4, 0)
+    1
+    """
+    "*** YOUR CODE HERE ***"
+    result, curr, i = 1, n, 0
+    while i < k:
+        curr = n - i
+        result = result * curr
+        i += 1
+    return result
+
+
+
+def sum_digits(y):
+    """Sum all the digits of y.
+
+    >>> sum_digits(10) # 1 + 0 = 1
+    1
+    >>> sum_digits(4224) # 4 + 2 + 2 + 4 = 12
+    12
+    >>> sum_digits(1234567890)
+    45
+    >>> a = sum_digits(123) # make sure that you are using return rather than print
+    >>> a
+    6
+    """
+    "*** YOUR CODE HERE ***"
+    next, curr, sum = y, 0, 0
+    while next > 0:
+        curr = next % 10
+        sum += curr
+        next = next // 10
+    return sum
+
+
+
+def double_eights(n):
+    """Return true if n has two eights in a row.
+    >>> double_eights(8)
+    False
+    >>> double_eights(88)
+    True
+    >>> double_eights(2882)
+    True
+    >>> double_eights(880088)
+    True
+    >>> double_eights(12345)
+    False
+    >>> double_eights(80808080)
+    False
+    """
+    "*** YOUR CODE HERE ***"
+    next, curr, last = n, 0, 0
+    while next > 0:
+        curr = next % 10
+        if curr == 8 and curr == last:
+            return True
+        next = next // 10
+        last = curr
+    return False     
+
