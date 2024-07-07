4630ce21b07934e821e6b3d3b3237a7dad8754c1
hello2333 <zn.hello2333@gmail.com>
2024-07-07 18:05:51 +0800

    [leetcode] 744: 用了二分法优化，结果发现时间性能并没有好很多:)

--- leetcode/744.find-smallest-letter-greater-than-target.py
+++ leetcode/744.find-smallest-letter-greater-than-target.py
@@ -5,12 +5,38 @@
 #
 
 # @lc code=start
+from typing import List
+
+
 class Solution:
     def nextGreatestLetter(self, letters: List[str], target: str) -> str:
-        for i in range(len(letters)):
-            if letters[i] > target:
-                return letters[i]
+        left = 0
+        right = len(letters) - 1
+        rightBounder = len(letters)
+        while left <= right and right < rightBounder and left >= 0:
+            middle = int((right - left) / 2) + left
+            # print("middle: ", middle, letters[middle], "left: ", left, "right: ", right)
+            if letters[middle] <= target:
+                # print("middle is smaller, go to right")
+                # if not has right part: break
+                if  middle + 1 >= rightBounder:
+                    break
+                # else continue with right part
+                left = middle + 1
+            if letters[middle] > target:
+                # print("middle is smaller, go to left")
+                # if not has left part: return middle
+                if middle - 1 < 0:
+                    return letters[middle]
+                # if max of left part smaller than target: return middle
+                if letters[middle - 1] <= target:
+                    return letters[middle]
+                # continue with left part
+                right = middle - 1
         return letters[0]
         
 # @lc code=end
+
+test = Solution()
+print(test.nextGreatestLetter(["c","f","j"], "g"))
 
