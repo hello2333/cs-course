6ec1d098ff81564f653cb7bbf3488f92c55d5ea6
hello2333 <zn.hello2333@gmail.com>
2024-08-12 08:49:24 +0800

    [nand2teris] 04.mul and 03 done

--- nand-to-tetris/nand2tetris/projects/04/mult/Mult.asm
+++ nand-to-tetris/nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,54 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+// V1
+// R2 = 0
+// LOOP:
+//   R2 = R2 + R0
+//   R1 = R1 -1
+//   if R1 > 0:
+//     go to LOOP
+// END:
+
+// V2
+// R2 = 0
+// LOOP:
+//   if R1 <= 0:
+//     go to END
+//   R2 = R2 + R0
+//   R1 = R1 -1
+// END:
+
+// V2
+// R2 = 0
+// LOOP:
+//   if R1 <= 0:
+//     go to END
+//   R2 = R2 + R0
+//   R1 = R1 -1
+//   go to LOOP
+// END:
+
+@R2
+M=0
+
+(LOOP)
+@R1
+D=M
+@END
+D;JLE
+
+@R0
+D=M
+@R2
+M=M+D
+
+@R1
+M=M-1
+
+@LOOP
+0;JMP
+
+(END)
+@END
+0;JMP
